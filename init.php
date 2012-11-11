f3221aa0839dd41586f4d9bcdf6dceeaafc635eb
Isra <isra00@gmail.com>
2012-11-11 23:09:46 +0100

    New init file

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+
+define('RECAPTCHA_PUB', 'your ReCaptcha public key');
+define('RECAPTCHA_PRIV', 'your ReCaptcha private key');
+define('TMP_DIR', 'temporary dir where uploaded files will be converted');

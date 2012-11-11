2a973a1a77523e95f071d4d487d63947004e343f
root <root@isra.(none)>
2012-11-11 22:42:26 +0100

    Hello world

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+include 'init.php';
+
+include 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>On-line .po compiler, convert your .po files into .mo - FREE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Convert your .po files into .mo on-line for free - online msgfmt">
+    <meta name="keywords" content="msgfmt, gettext, gnu gettext, convert, online, free, po, mo, po file, mo file, po converter">
+    <meta name="author" content="Israel Viana">
+
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <style>
+    .captcha { margin: 1em 0; display: none; }
+    .submit { margin-top: 1em; }
+    .footer { text-align: center; }
+    </style>
+    <script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-6329572-2']);
+      _gaq.push(['_trackPageview']);
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    </script>
+  </head>
+  <body>
+    <div class="hero-unit">
+      <h1>Convert .po files into .mo</h1>
+      <p>Quick, easy and for free</p>
+      <form>
+        <div class="form-actions">
+          <label for="up">Upload your .po file (max. 1MB)</label>
+          <input type="file" name="up" id="up" />
+          
+          <div class="captcha">
+            <script type="text/javascript" src="http://www.google.com/recaptcha/api/challenge?k=<?php print RECAPTCHA_PUB ?>"></script>
+            <noscript>
+              <iframe src="http://www.google.com/recaptcha/api/noscript?k=<?php print RECAPTCHA_PUB ?>" height="300" width="500" frameborder="0"></iframe><br>
+              <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+              <input type="hidden" name="recaptcha_response_field" value="manual_challenge">
+            </noscript>
+          </div>
+          
+          <div class="submit">
+            <button type="submit" class="btn btn-success">Convert to .mo</button>
+          </div>
+        </div>
+      </form>
+    </div>
+    
+    <div class="footer">
+        <p>Brought to you by <a href="http://israelviana.es/?utm_source=PoConverter&utm_medium=FooterLink&utm_campaign=PoConverter">Israel Viana</p>
+    </div>
+    
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script>
+    $(function() {
+        $('#up').live('change', function() {
+            $(".captcha").show();
+        });
+    });
+    </script>
+  </body>
+</html>

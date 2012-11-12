c8d740d3da2d6492f9b4f4bf20c3e67552320f57
Isra <isra00@gmail.com>
2012-11-12 11:12:56 +0100

    Add social widgets and explanation text

--- index.view.php
+++ index.view.php
@@ -11,12 +11,20 @@
     <link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
     <style>
+      h1 { overflow: hidden; }
+      h1 span:first-child { float: left; }
+      h1 .tweet-button { float: left; margin: 15px; }
+      h1 .fb-like { float: left; margin: 26px 0 0 30px; }
+	  h1 .tweet { float: left; margin-left: 20px; }
+      h4 { margin-bottom: 2em; }
       .captcha { margin: 1em 0; display: none; }
       .submit { margin-top: 1em; }
       .footer { text-align: center; }
       .download-page { text-align: center; }
       .download { margin-top: 2em; }
       .download .btn { padding: 15px 25px; }
+      .explanation h3 { margin-top: 0; }
+      .explanation p { font-size: .8em; line-height: 1.6em; }
     </style>
     <script type="text/javascript">
       var _gaq = _gaq || [];
@@ -30,20 +38,45 @@
     </script>
   </head>
   <body>
-  
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) return;
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/es_ES/all.js#xfbml=1";
+     fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
     <?php if (isset($deliver)) : ?>
     
     <div class="hero-unit download-page">
-      <h1>Done! Here you have it</h1>
+      <h1>
+          <span>Done! Here you have it</span>
+		  <div class="fb-like" data-href="http://online-po-compile.israelviana.es/?utm_source=Share&amp;utm_medium=FB+Like+after+convert&amp;utm_campaign=Share" data-send="false" data-layout="button_count" data-width="80" data-show-faces="false"></div>
+		  <span class="tweet">
+			  <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://online-po-compile.israelviana.es/?utm_source=Share&amp;utm_medium=Tweet+after+convert&amp;utm_campaign=Share" data-via="ivianag" data-size="large" data-hashtags="i18n">Tweet</a></span>
+			  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		  </span>
+      </h1>
+
       <p class="download"><a class="btn btn-large btn-success" href="<?php print $download_url ?>">Download your compiled .mo file</a></p>
     </div>
     
     <?php else : ?>
   
     <div class="hero-unit">
-      <h1>Convert .po files into .mo</h1>
-      <p>Quick, easy and for free</p>
-      <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>" enctype="multipart/form-data">
+      <h1>
+          <span>Convert .po files into .mo</span>
+		  <div class="fb-like" data-href="http://online-po-compile.israelviana.es/?utm_source=Share&amp;utm_medium=FB+Like+before+convert&amp;utm_campaign=Share" data-send="false" data-layout="button_count" data-width="80" data-show-faces="false"></div>
+		  <span class="tweet">
+			  <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://online-po-compile.israelviana.es/?utm_source=Share&amp;utm_medium=Tweet+after+convert&amp;utm_campaign=Share" data-via="ivianag" data-size="large" data-hashtags="i18n">Tweet</a></span>
+			  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		  </span>
+      </h1>
+
+      <h4>Quick, easy and for free</h4>
+      <form method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>" enctype="multipart/form-data" class="row">
       
         <?php if (!empty($errors)) : ?>
         <div class="alert alert-error">
@@ -53,7 +86,7 @@
         </div>
         <?php endif ?>
       
-        <div class="form-actions">
+        <div class="span6 well">
           <label for="up">Upload your .po file (max. <?php print formatRawSize(MAX_FILE_SIZE) ?>)</label>
           <input type="file" name="up" id="up" />
           
@@ -69,13 +102,20 @@
           <div class="submit">
             <button type="submit" id="submit" name="submit" class="btn btn-primary">Convert file to .mo</button>
           </div>
-        </div><!-- /.form-actions -->
+        </div><!-- /.well -->
+
+        <div class="span5 offset1 explanation">
+            <h3>.po and .mo files?</h3>
+            <p>.po is the file format used by GNU gettext translation system. To use it, you need to compile the .po files in a more efficient format, the .po ones. Here you can compile .po files for free.</p>
+            <!-- .po es el formato de fichero usado por el sistema de traducción GNU gettext. Para poder usarlo en un programa es necesario compilar los ficheros .po en un formato más eficiente, los .mo. Aquí puedes compilar ficheros .po gratis. -->
+        </div>
+
       </form>
     </div>
     <?php endif ?>
     
     <div class="footer">
-        <p>Brought to you by <a href="http://israelviana.es/?utm_source=PoConverter&amp;utm_medium=FooterLink&amp;utm_campaign=PoConverter">Israel Viana</a></p>
+        <p>Brought to you by <a href="http://israelviana.es/?utm_source=PoCompiler&amp;utm_medium=FooterLink&amp;utm_campaign=PoCompiler">Israel Viana</a></p>
     </div>
     
     <a href="https://github.com/isra00/online-po-compile"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="This site is Free Software"></a>
@@ -92,6 +132,11 @@
             if ($("#up").val().length == 0) {
                 alert("Please select a .po file to upload");
                 e.preventDefault();
+            } else {
+                if ($("#recaptcha_response_field").val().length == 0) {
+                    alert("Please enter the anti-robots text");
+                    e.preventDefault();
+                }
             }
         });
     });

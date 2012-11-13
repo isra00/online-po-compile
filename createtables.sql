6eb9d47ea77d50b82df8e8a7982f3f259cfd6eb3
Isra <isra00@gmail.com>
2012-11-14 00:00:41 +0100

    Remove downloaded field, since we cannot modify records when using ARCHIVE engine. Download metrics can be collected from logs.

--- createtables.sql
+++ createtables.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS `pocompiler_log` (
   `filename` varchar(50) NOT NULL,
   `compilation_time` float NOT NULL,
   `filesize` smallint(5) unsigned NOT NULL,
-  `downloaded` tinyint(3) unsigned NOT NULL,
   `file_id` char(32) NOT NULL
 ) ENGINE=ARCHIVE DEFAULT CHARSET=utf8;

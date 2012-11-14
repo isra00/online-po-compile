ee5f88883a7bd0cefd4fb44e22aa5868061d0d5f
Isra <isra00@gmail.com>
2012-11-15 00:11:13 +0100

    Added filesize_po file and renamed filesize to filesize_mo

--- createtables.sql
+++ createtables.sql
@@ -1,7 +1,8 @@
-CREATE TABLE IF NOT EXISTS `pocompiler_log` (
+CREATE TABLE `pocompiler_log` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `filename` varchar(50) NOT NULL,
   `compilation_time` float NOT NULL,
-  `filesize` smallint(5) unsigned NOT NULL,
+  `filesize_po` smallint(5) unsigned NOT NULL,
+  `filesize_mo` smallint(5) unsigned NOT NULL,
   `file_id` char(32) NOT NULL
 ) ENGINE=ARCHIVE DEFAULT CHARSET=utf8;
